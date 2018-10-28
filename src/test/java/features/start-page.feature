715daf0f766c6b669ea3f124f69958ef77cf1fa1
dpinchuk <pinchuk.dap@gmail.com>
2018-10-28 21:25:06 +0200

    in progress

--- src/test/java/features/start-page.feature
+++ src/test/java/features/start-page.feature
@@ -2,18 +2,37 @@ Feature: Open Main Page
   This feature opens main web page
 
   @hotel_search_0
-  Scenario: Open main page 1
-    Given Open main web page
+  Scenario: Open main page
+    Given Opened main web page
     And Find search field
-    And Input data into the field
-    And Press search button
-    Then Open search page
+    And Input data into the city field
+    |city                                         |
+    |New York, New York, United States Of America |
 
-  @hotel_search_1
-  Scenario: Open main page 2
-    Given Open main web page
-    And Find search field
-    And Input data into the field
-    And
+    And Input check in date tomorrow
+
+    And Point count of check out days
+    |days |
+    |7    |
+
+    #And Click on Edit or add rooms button
+    And Click on Rooms list and select More options
+
+    And Select count of Rooms
+    |rooms |
+    |1     |
+
+    And Select count of Adults
+    |adults |
+    |1      |
+
+    And Select number of Children
+    |children |
+    |2        |
+
+    And Select age of Children
+    |age1 |age2  |
+    |2    |3     |
+
     And Press search button
-    Then Open search page
+    Then Opened search page
--- src/test/java/stepdefinitions/OpenPageSD.java
+++ src/test/java/stepdefinitions/OpenPageSD.java
@@ -1,6 +1,7 @@
 package stepdefinitions;
 
 import controllers.MainController;
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -9,6 +10,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import static configuration.Configuration.DATE_FORMAT;
 import static configuration.Configuration.DAYS;
 import static configuration.Configuration.TEST_CITY;
 import static org.junit.Assert.assertEquals;
@@ -18,7 +26,7 @@ public class OpenPageSD {
     private MainController mainController;
     private WebElement webElement;
 
-    @Given("^Open main web page$")
+    @Given("^Opened main web page$")
     public void openMainWebPage() throws Throwable {
         this.mainController = new MainController();
         this.mainController.initTestEnvironment();
@@ -30,9 +38,11 @@ public class OpenPageSD {
         this.webElement.clear();
     }
 
-    @And("^Input data into the field$")
-    public void inputDataIntoTheField() throws Throwable {
-        this.webElement.sendKeys(TEST_CITY);
+    @And("^Input data into the city field$")
+    public void inputDataIntoTheField(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        String dataRaw = data.get(1).get(0);
+        this.webElement.sendKeys(dataRaw);
         this.webElement.sendKeys(Keys.TAB);
     }
 
@@ -42,9 +52,100 @@ public class OpenPageSD {
         this.webElement.click();
     }
 
-    @Then("^Open search page$")
+    @And("^Input check in date tomorrow$")
+    public void inputCheckInDate() throws Throwable {
+        Calendar calendar = new GregorianCalendar();
+        DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        String date = dateFormat.format(calendar.getTime());
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-localised-check-in']"));
+        this.webElement.clear();
+        this.webElement.sendKeys(date);
+        //this.webElement.sendKeys(Keys.TAB);
+    }
+
+    @And("^Point count of check out days$")
+    public void pointCountOfCheckOutDays(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        String dataRaw = data.get(1).get(0);
+        int days = Integer.parseInt(dataRaw);
+        Calendar calendar = new GregorianCalendar();
+        DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        calendar.add(Calendar.DAY_OF_YEAR, days + 1);
+        String date = dateFormat.format(calendar.getTime());
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-localised-check-out']"));
+        this.webElement.clear();
+        this.webElement.sendKeys(date);
+        this.webElement.sendKeys(Keys.TAB);
+    }
+
+    @And("^Click on Edit or add rooms button$")
+    public void clickOnEditOrAddRoomsButton() throws Throwable {
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='main-content']/div/div/div[1]/div/div/div/div[1]/div/form/div[4]/button"));
+        this.webElement.click();
+    }
+
+    @And("^Click on Rooms list and select More options$")
+    public void clickOnRoomsListAndSelectMoreOptions() throws Throwable {
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-compact-occupancy']"));
+        this.webElement.click();
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-compact-occupancy']/option[3]"));
+        this.webElement.click();
+    }
+
+    @And("^Select count of Rooms$")
+    public void selectCountOfRooms(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        int rooms = Integer.parseInt(data.get(1).get(0));
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-rooms']"));
+        this.webElement.click();
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-rooms']/option[" + rooms + "]"));
+        this.webElement.click();
+        //this.webElement.sendKeys(Keys.TAB);
+
+    }
+
+    @And("^Select count of Adults$")
+    public void selectCountOfAdults(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        String adults = data.get(1).get(0);
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-room-0-adults']"));
+        this.webElement.click();
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-room-0-adults']/option[" + adults + "]"));
+        this.webElement.click();
+    }
+
+    @And("^Select number of Children$")
+    public void selectNumberSOfChildren(DataTable table) throws Throwable {//*[@id="qf-0q-room-0-children"]/option[3]
+        List<List<String>> data = table.raw();
+        int children = Integer.parseInt(data.get(1).get(0));
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id=\"qf-0q-room-0-children\"]"));
+        this.webElement.click();
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id=\"qf-0q-room-0-children\"]/option[" + (children + 1) + "]"));
+        this.webElement.click();
+    }
+
+    @And("^Select age of Children$")
+    public void selectAgeSOfChildren(DataTable table) throws Throwable {
+        List<List<String>> data = table.raw();
+        int childAge1 = Integer.parseInt(data.get(1).get(0));
+        int childAge2 = Integer.parseInt(data.get(1).get(1));
+
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-room-0-child-0-age']"));
+        this.webElement.click();
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-room-0-child-0-age']/option[" + (childAge1 + 2) + "]"));
+        this.webElement.click();
+
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-room-0-child-1-age']"));
+        this.webElement.click();
+        this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='qf-0q-room-0-child-1-age']/option[" + (childAge2 + 2) + "]"));
+        this.webElement.click();
+    }
+
+    @Then("^Opened search page$")
     public void openSearchPage() throws Throwable {
         this.webElement = this.mainController.getDriver().findElement(By.xpath("//*[@id='search']/div[1]/div/h1"));
         assertEquals(TEST_CITY, this.webElement.getText());
     }
+
 }
